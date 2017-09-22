7407ed070fbf6ad0f397a3deb7e95168e0b34be5
Pramod Jacob <psjacob68@gmail.com>
2017-09-21 22:58:59 -0400

    Added instructions for setting up cron jobs

--- wallpaper_changer.sh
+++ wallpaper_changer.sh
@@ -32,3 +32,7 @@ echo "Wallpaper file path: ${WP_PATH}"
 # Set wallpaper
 echo "Rendering ${WP_PATH}"
 eval "gsettings set org.gnome.desktop.background picture-uri 'file:///${WP_PATH}'"
+
+# Add the following to cron jobs (crontab -e) to run job every 3 hours & log to /tmp/cron.log
+# DISPLAY=:0
+# 0 */3 * * * /home/pjacob/Dropbox/programming/scripts/wallpaper_changer.sh >> /tmp/cron.log 2>&1
